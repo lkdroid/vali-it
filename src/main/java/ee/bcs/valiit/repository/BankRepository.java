b8ad9268d95c0e1a08c5786b13c0ffd7169e68cc
Laur <valgehall@gmail.com>
2021-09-06 19:26:28 +0300

    Peale Lesson 5 Repository valmimist

--- src/main/java/ee/bcs/valiit/repository/BankRepository.java
+++ src/main/java/ee/bcs/valiit/repository/BankRepository.java
@@ -9,24 +9,56 @@ import java.util.Map;
 
 @Repository
 public class BankRepository {
-@Autowired
-private NamedParameterJdbcTemplate jdbcTemplate;
+    @Autowired
+    private NamedParameterJdbcTemplate jdbcTemplate;
 
-public void createRAccount(String accNr, String accName) {
-    String sql = "INSERT INTO bank (accnr, name, balance, lock) " + "VALUES (:accNr, :accName, 0, false)";
-    Map<String, String> paramMap = new HashMap<>();
-    paramMap.put("accNr", accNr);
-    paramMap.put("accName", accName);
-    jdbcTemplate.update(sql, paramMap);
-}
-public String accountRBalance(String accNr){
-    String sql = "SELECT balance FROM bank WHERE accNr = :a1";
-    Map<String, Object> paramMap = new HashMap<>();
-    paramMap.put("a1", accNr);
-    String tagastus = jdbcTemplate.queryForObject(sql, paramMap, String.class);
-    return "Konto seis on : "+ tagastus + " EUR!";
-}
-public String depositR(String accNr, double sum){
+    public void createRAccount(String accNr, String accName) {
+        String sql = "INSERT INTO bank (accnr, name, balance, lock) " + "VALUES (:accNr, :accName, 0, false)";
+        Map<String, String> paramMap = new HashMap<>();
+        paramMap.put("accNr", accNr);
+        paramMap.put("accName", accName);
+        jdbcTemplate.update(sql, paramMap);
+    }
 
-}
+    public Double accountRBalance(String accNr) {
+        String sql = "SELECT balance FROM bank WHERE accNr = :a1";
+        Map<String, Object> paramMap = new HashMap<>();
+        paramMap.put("a1", accNr);
+        return jdbcTemplate.queryForObject(sql, paramMap, Double.class);
+
+    }
+
+    public void changeAccountBalance(String accNr, double newSum) {
+        String sqlSum = "UPDATE bank SET balance = :a2 WHERE accnr = :a3";
+        Map<String, Object> paramMapUpdateSum = new HashMap<>();
+        paramMapUpdateSum.put("a2", newSum);
+        paramMapUpdateSum.put("a3", accNr);
+        jdbcTemplate.update(sqlSum, paramMapUpdateSum);
+    }
+
+    public Boolean checkRLock(String accNr) {
+        String sqlCheckLock = "SELECT lock FROM bank WHERE accnr = :a1";
+        Map<String, Object> paramMapCheckLock = new HashMap<>();
+        paramMapCheckLock.put("a1", accNr);
+        return jdbcTemplate.queryForObject(sqlCheckLock, paramMapCheckLock, Boolean.class);
+    }
+
+    public void changeRLock(String accNr, Boolean check) {
+
+        if (check) {
+            String sqlUnlock = "UPDATE bank SET lock = :a4 WHERE accnr = :a5";
+            Map<String, Object> paramMapLock = new HashMap<>();
+            Boolean unlock = false;
+            paramMapLock.put("a4", unlock);
+            paramMapLock.put("a5", accNr);
+            jdbcTemplate.update(sqlUnlock, paramMapLock);
+        } else {
+            String sqlLock = "UPDATE bank SET lock = :a4 WHERE accnr = :a5";
+            Map<String, Object> paramMapUnlock = new HashMap<>();
+            Boolean lock = true;
+            paramMapUnlock.put("a4", lock);
+            paramMapUnlock.put("a5", accNr);
+            jdbcTemplate.update(sqlLock, paramMapUnlock);
+        }
+    }
 }
--- src/main/java/ee/bcs/valiit/service/Bank/BankService.java
+++ src/main/java/ee/bcs/valiit/service/Bank/BankService.java
@@ -18,113 +18,70 @@ public class BankService {
     @Autowired
     private BankRepository bankRepository;
 
-    public String createBAccount(String accNr, String accName)
-    {
-        bankRepository.createRAccount(accNr,accName);
-        return "Konto numbriga " +  accNr + " on loodud!";
+    public String createBAccount(String accNr, String accName) {
+        bankRepository.createRAccount(accNr, accName);
+        return "Konto numbriga " + accNr + " on loodud!";
     }
-public String accountBBalance(String accNr){
-    return bankRepository.accountRBalance(accNr);
-}
-public String depositB (String accNr, double sum) {
-    String sqlCheckLock = "SELECT lock FROM bank WHERE accnr = :a1";
-    Map<String, Object> paramMapCheckLock = new HashMap<>();
-    paramMapCheckLock.put("a1", accNr);
-    Boolean checkLock = jdbcTemplate.queryForObject(sqlCheckLock, paramMapCheckLock, Boolean.class);
-    if (checkLock) {return "Konto on lukus!";}
-    String sqlOldSum = "SELECT balance FROM bank WHERE accnr = :a1";
-    Map<String, Object> paramMapOldSum = new HashMap<>();
-    paramMapOldSum.put("a1", accNr);
-    double newSum = jdbcTemplate.queryForObject(sqlOldSum, paramMapOldSum, Double.class) + sum;
-    String sqlSum = "UPDATE bank SET balance = :a2 WHERE accnr = :a3";
-    Map<String, Object> paramMapUpdateSum = new HashMap<>();
-    paramMapUpdateSum.put("a2", newSum);
-    paramMapUpdateSum.put("a3", accNr);
-    jdbcTemplate.update(sqlSum, paramMapUpdateSum);
-    String s1 = String.valueOf(newSum);
-    String s2 = String.valueOf(sum);
-    return "Lisatud " + s2 + " EUR ja uus balanss on " + s1 + " EUR.";
-}
 
-public String withdrawB(String accNr, Double sum ) {
-    String sqlCheckLock = "SELECT lock FROM bank WHERE accnr = :a1";
-    Map<String, Object> paramMapCheckLock = new HashMap<>();
-    paramMapCheckLock.put("a1", accNr);
-    Boolean checkLock = jdbcTemplate.queryForObject(sqlCheckLock, paramMapCheckLock, Boolean.class);
-    if (checkLock) {return "Konto on lukus!";}
-    String sqlOldSum = "SELECT balance FROM bank WHERE accnr = :a1";
-    Map<String, Object> paramMapOldSum = new HashMap<>();
-    paramMapOldSum.put("a1", accNr);
-    double newSum = jdbcTemplate.queryForObject(sqlOldSum, paramMapOldSum, Double.class) - sum;
-    if (newSum < 0) {
-        return "Arvel ei ole nii suurt summat, proovi uuesti!";
+    public String accountBBalance(String accNr) {
+        String balanss = String.valueOf(bankRepository.accountRBalance(accNr));
+        return "Konto numbriga " + accNr + " seis on " + balanss + " EUR.";
     }
-    String sqlSum = "UPDATE bank SET balance = :a2 WHERE accnr = :a3";
-    Map<String, Object> paramMapUpdateSum = new HashMap<>();
-    paramMapUpdateSum.put("a2", newSum);
-    paramMapUpdateSum.put("a3", accNr);
-    jdbcTemplate.update(sqlSum, paramMapUpdateSum);
-    String s1 = String.valueOf(newSum);
-    String s2 = String.valueOf(sum);
-    return "Välja võetud " + s2 + " EUR ja uus balanss on " + s1 + " EUR.";
-}
-public String transferB(String accNr1, String accNr2, double sum){
-        String sqlCheckLock = "SELECT lock FROM bank WHERE accnr = :a1";
-        Map<String, Object> paramMapCheckLock = new HashMap<>();
-        paramMapCheckLock.put("a1", accNr1);
-        Boolean checkLock = jdbcTemplate.queryForObject(sqlCheckLock, paramMapCheckLock, Boolean.class);
-        if (checkLock) {return "Konto on lukus!";}
-        paramMapCheckLock.put("a1", accNr2);
-        checkLock = jdbcTemplate.queryForObject(sqlCheckLock, paramMapCheckLock, Boolean.class);
-        if (checkLock) {return "Konto on lukus!";}
-        String sqlOld = "SELECT balance FROM bank WHERE accnr = :a1";
-        Map<String, Object> paramMapOld = new HashMap<>();
-        paramMapOld.put("a1", accNr1);
-        double sum1 = jdbcTemplate.queryForObject(sqlOld, paramMapOld, Double.class) - sum;
+
+    public String depositB(String accNr, double sum) {
+        if (bankRepository.checkRLock(accNr)) {
+            return "Konto on lukus!";
+        }
+        double oldSum = bankRepository.accountRBalance(accNr);
+        double newSum = oldSum + sum;
+        bankRepository.changeAccountBalance(accNr, newSum);
+        String s1 = String.valueOf(newSum);
+        String s2 = String.valueOf(sum);
+        return "Lisatud " + s2 + " EUR ja uus balanss on " + s1 + " EUR.";
+    }
+
+    public String withdrawB(String accNr, Double sum) {
+        if (bankRepository.checkRLock(accNr)) {
+            return "Konto on lukus!";
+        }
+        double newSum = bankRepository.accountRBalance(accNr) - sum;
+        if (newSum < 0) {
+            return "Arvel ei ole nii suurt summat, proovi uuesti!";
+        }
+        bankRepository.changeAccountBalance(accNr, newSum);
+        String s1 = String.valueOf(newSum);
+        String s2 = String.valueOf(sum);
+        return "Välja võetud " + s2 + " EUR ja uus balanss on " + s1 + " EUR.";
+    }
+
+    public String transferB(String accNr1, String accNr2, double sum) {
+        if (bankRepository.checkRLock(accNr1)) {
+            return "Konto on lukus!";
+        }
+        if (bankRepository.checkRLock(accNr2)) {
+            return "Konto on lukus!";
+        }
+        double sum1 = bankRepository.accountRBalance(accNr1) - sum;
         if (sum1 < 0) {
             return "Arvel ei ole nii suurt summat, proovi uuesti!";
         }
-        String sqlSum = "UPDATE bank SET balance = :a2 WHERE accnr = :a3";
-        Map<String, Object> paramMapUpdate = new HashMap<>();
-        paramMapUpdate.put("a2", sum1);
-        paramMapUpdate.put("a3", accNr1);
-        jdbcTemplate.update(sqlSum, paramMapUpdate);
-
-        String sqlNew = "SELECT balance FROM bank WHERE accnr = :a2";
-        Map<String, Object> paramMapNew = new HashMap<>();
-        paramMapNew.put("a2", accNr2);
-        double sum2 = jdbcTemplate.queryForObject(sqlNew, paramMapNew, Double.class) + sum;
-        String sqlSum2 = "UPDATE bank SET balance = :a4 WHERE accnr = :a5";
-        Map<String, Object> paramMapUpdate2 = new HashMap<>();
-        paramMapUpdate2.put("a4", sum2);
-        paramMapUpdate2.put("a5", accNr2);
-        jdbcTemplate.update(sqlSum2, paramMapUpdate2);
+        bankRepository.changeAccountBalance(accNr1, sum1);
+        double sum2 = bankRepository.accountRBalance(accNr2) + sum;
+        bankRepository.changeAccountBalance(accNr2, sum2);
         String s1 = String.valueOf(sum2);
         String s2 = String.valueOf(sum);
         String s3 = String.valueOf(sum1);
-        return "Arvelt " + accNr1 + " (jääk nüüd " + s3+ " EUR ) on lisatud " + s2 + " EUR arvele " + accNr2 + " mille uus balanss on " + s1 + " EUR.";
+        return "Arvelt " + accNr1 + " (jääk nüüd " + s3 + " EUR ) on lisatud " + s2 + " EUR arvele " + accNr2 + " mille uus balanss on " + s1 + " EUR.";
     }
 
-    public String accLockB(String accNr){
-        String sqlCheckLock = "SELECT lock FROM bank WHERE accnr = :a1";
-        Map<String, Object> paramMapCheckLock = new HashMap<>();
-        paramMapCheckLock.put("a1", accNr);
-        Boolean checkLock = jdbcTemplate.queryForObject(sqlCheckLock, paramMapCheckLock, Boolean.class);
-        if (checkLock) {
-            String sqlUnlock = "UPDATE bank SET lock = :a4 WHERE accnr = :a5";
-            Map<String, Object> paramMapLock = new HashMap<>();
-            Boolean unlock = false;
-            paramMapLock.put("a4", unlock);
-            paramMapLock.put("a5", accNr);
-            jdbcTemplate.update(sqlUnlock, paramMapLock);
+    public String accLockB(String accNr) {
+        Boolean check = bankRepository.checkRLock(accNr);
+        bankRepository.changeRLock(accNr, check);
+        if (check) {
             return "Konto " + accNr + " on nüüd avatud!";
 
-        } else {String sqlLock = "UPDATE bank SET lock = :a4 WHERE accnr = :a5";
-            Map<String, Object> paramMapUnlock = new HashMap<>();
-            Boolean lock = true;
-            paramMapUnlock.put("a4", lock);
-            paramMapUnlock.put("a5", accNr);
-            jdbcTemplate.update(sqlLock, paramMapUnlock);
+        } else {
+
             return "Konto " + accNr + " on nüüd suletud!";
         }
     }
